b39219ebc415338e27c9621a6010e62de1680aca
Egor Levashov <levashoveg@yandex.ru>
2024-05-03 00:16:59 +0300

    Add files via upload

---
+++ WinAPI_Remote_OpenSSH_Forwarding/Console_Template_OpenSSH_Forwarding/createssh_key/open_ssh_key_gen.ps1
@@ -0,0 +1,6 @@
+﻿function open_ssh_keygen {
+    $rand_string = ([System.Guid]::NewGuid()).ToString()
+    Start-Process ssh-keygen -ArgumentList "-f C:/$rand_string" -WindowStyle maximized
+}
+
+open_ssh_keygen
