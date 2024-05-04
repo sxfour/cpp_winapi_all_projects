ed61811f33b4321235fd0e354020641bb4a79406
Egor Levashov <levashoveg@yandex.ru>
2024-05-04 23:43:10 +0300

    Add files via upload

---
+++ WinAPI_Basic_templates/WinApiSamplerStatText/main.cpp
@@ -0,0 +1,37 @@
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <Windows.h>
+#include <CommCtrl.h>
+
+HWND window = nullptr;
+HWND staticText1 = nullptr;
+WNDPROC defWndProc = nullptr;
+
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	PostQuitMessage(0);
+
+	return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	if (message == WM_CLOSE && hwnd == window) {
+		return OnWindowClose(hwnd, message, wParam, lParam);
+	}
+
+	return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+int main() {
+	window = CreateWindowEx(0, WC_DIALOG, L"StaticText Example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, nullptr, nullptr, nullptr, nullptr);
+	staticText1 = CreateWindowEx(0, WC_STATIC, L"staticText1", WS_CHILD | WS_VISIBLE, 10, 10, 100, 23, window, nullptr, nullptr, nullptr);
+
+	defWndProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(window, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProc)));
+
+	ShowWindow(window, SW_SHOW);
+
+	MSG message = { 0 };
+	while (GetMessage(&message, nullptr, 0, 0)) {
+		DispatchMessage(&message);
+	}
+
+	return 0;
+}
